f48a4841ff1fbf3a4fb375c5a850605fe47bb361
Florent LOTTIN <flottin@gmail.com>
2024-02-27 23:35:44 +0100

    Update Game.js

--- Game.js
+++ Game.js
@@ -21,9 +21,9 @@ function draw ()  {
 window.requestAnimationFrame(draw);
 
 canvas.addEventListener("click", function(){
-    bullets.push(new Bullet(170, 420)) 
+    bullets.push(new Bullet(170, 440)) 
 });
 
 canvas.addEventListener("touchstart", function(){
-    bullets.push(new Bullet(170, 420)) 
+    bullets.push(new Bullet(170, 440)) 
 });

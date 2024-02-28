e119bd65bff28edf9ce8a4d3013d5cc0cc017130
Florent Lottin <f.lottin@bilendi.com>
2024-02-28 15:22:37 +0100

    Add Enemy

---
+++ Enemy.js
@@ -0,0 +1,35 @@
+class Enemy {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  animate() {
+    this.y = this.y - 6;
+  }
+
+  draw() {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+    ctx.save();
+    ctx.shadowColor = "green";
+    ctx.shadowBlur = 15;
+    ctx.beginPath();
+    ctx.restore();
+
+    var sWidth = canvas.width;
+    var sHeight = canvas.height;
+    var path = new Path2D();
+    path.moveTo(sWidth / 2 + 40, 500);
+    path.lineTo(sWidth / 2, 10);
+    path.lineTo(sWidth / 2 - 40, 15);
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.fill(path);
+    ctx.closePath();
+  }
+
+  getY() {
+    return this.y;
+  }
+}

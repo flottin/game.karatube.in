a80b46e5971a7accb280a00c74c4433b7bf7d717
Florent Lottin <f.lottin@bilendi.com>
2024-02-28 21:34:40 +0100

    Add damage

--- Enemy.js
+++ Enemy.js
@@ -2,26 +2,59 @@ class Enemy {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    var sWidth = canvas.width;
+    var sHeight = canvas.height;
+    this.ax = sWidth / 2 + 40;
+    this.ay = 20;
+
+    this.bx = sWidth / 2;
+    this.by = 50;
+
+    this.cx = sWidth / 2 - 40;
+    this.cy = 20;
+
+    this.damage = false;
   }
 
   animate() {
     this.y = this.y - 6;
   }
 
+  getCollision() {
+    bullets.forEach((bullet) => {
+      if (typeof bullet.x !== "undefined") {
+        if (bullet.y < this.by + 10) {
+          this.damage = true;
+        }
+      }
+    });
+  }
+
   draw() {
+    this.getCollision();
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
     ctx.save();
-    ctx.shadowColor = "red";
+
+    if (this.damage === true) {
+      this.damage = false;
+      ctx.shadowColor = "yellow";
+    } else {
+      ctx.shadowColor = "red";
+    }
+
     ctx.shadowBlur = 15;
     ctx.beginPath();
 
-    var sWidth = canvas.width;
-    var sHeight = canvas.height;
     var path = new Path2D();
-    path.moveTo(sWidth / 2 + 40, 20);
-    path.lineTo(sWidth / 2, 50);
-    path.lineTo(sWidth / 2 - 40, 20);
+
+    path.moveTo(this.ax, this.ay);
+    path.lineTo(this.bx, this.by);
+    path.lineTo(this.cx, this.cy);
     ctx.beginPath();
     ctx.fillStyle = "black";
     ctx.fill(path);
